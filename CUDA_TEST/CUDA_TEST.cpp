b6b42c9e7e5776283838392911bb9fc7a59097d2
Kerry-cho <cdh0012@naver.com>
2020-03-31 21:40:12 +0900

    Initialize

---
+++ CUDA_TEST/CUDA_TEST.cpp
@@ -0,0 +1,10 @@
+﻿#include <iostream>
+#include "vision_cuda.h"
+#include <torch/torch.h>
+
+int main()
+{
+    torch::Tensor t = torch::rand({ 224,224,3 });
+    //아래 코드는 동작안함 메트릭스 구조가 다르기때문에 호출이 가능 하다는 예시로 생각하세요.
+    ROIPool_forward_cuda(t, t, 0.7, 7, 7);
+}
